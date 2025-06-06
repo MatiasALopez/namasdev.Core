379b720c401e8ce043a806557b307e6d8485a14a
Matias Lopez <matutelopez@hotmail.com>
2025-06-06 09:44:24 -0300

    [main] (Types) Fix NumerosHelper

--- namasdev.Core/Types/NumerosHelper.cs
+++ namasdev.Core/Types/NumerosHelper.cs
@@ -2,7 +2,7 @@
 {
     public class NumerosHelper
     {
-        protected string CrearNumeroDesdeDigitos(int digitosEnteros, int digitosDecimales)
+        public static string CrearNumeroDesdeDigitos(int digitosEnteros, int digitosDecimales)
         {
             return $"{new string('9', digitosEnteros)}.{new string('9', digitosDecimales)}";
         }
