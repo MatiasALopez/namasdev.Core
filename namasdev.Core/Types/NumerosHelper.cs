b5a9e76132ff178459e71a4d8e86a19b730493b9
Matias Lopez <matutelopez@hotmail.com>
2025-06-06 09:41:51 -0300

    [main] (Types) Agregar: NumerosHelper

---
+++ namasdev.Core/Types/NumerosHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace namasdev.Core.Types
+{
+    public class NumerosHelper
+    {
+        protected string CrearNumeroDesdeDigitos(int digitosEnteros, int digitosDecimales)
+        {
+            return $"{new string('9', digitosEnteros)}.{new string('9', digitosDecimales)}";
+        }
+    }
+}
